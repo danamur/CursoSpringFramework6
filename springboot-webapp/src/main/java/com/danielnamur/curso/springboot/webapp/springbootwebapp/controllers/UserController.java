c898b7b8e3647b09d58dcb6cb01b671977bde232
Danamur <d.namur.n@gmail.com>
2024-01-27 12:17:34 -0300

    Pasando datos del controllador a la vista por medio de model con thymleaf

--- springboot-webapp/src/main/java/com/danielnamur/curso/springboot/webapp/springbootwebapp/controllers/UserController.java
+++ springboot-webapp/src/main/java/com/danielnamur/curso/springboot/webapp/springbootwebapp/controllers/UserController.java
@@ -1,13 +1,18 @@
 package com.danielnamur.curso.springboot.webapp.springbootwebapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
     
     @GetMapping("/details")
-    public String details() {
+    public String details(Model model) {
+        model.addAttribute("title", "Curso Spring Boot");
+        model.addAttribute("name", "Daniel");
+        model.addAttribute("lastname", "Namur");
+        
         return "details";
     }
 
