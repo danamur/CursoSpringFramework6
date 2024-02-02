d5b43f9024430323e0e570cea6072dac59fd4548
Danamur <d.namur.n@gmail.com>
2024-02-02 12:27:23 -0300

    Creando nueva api rest de listar usuarios, utilizando tanto el metodo arraay list como arrays.asList

--- springboot-webapp/src/main/java/com/danielnamur/curso/springboot/webapp/springbootwebapp/controllers/UserRestController.java
+++ springboot-webapp/src/main/java/com/danielnamur/curso/springboot/webapp/springbootwebapp/controllers/UserRestController.java
@@ -1,7 +1,11 @@
 package com.danielnamur.curso.springboot.webapp.springbootwebapp.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// import java.util.HashMap;
+// import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import models.User;
 import models.dto.UserDto;
+
 
 @RestController
 @RequestMapping("/api")
@@ -29,4 +34,20 @@ public class UserRestController {
         return userDto;
     }
 
+    @GetMapping("/users")
+    public List<User> List() {
+        User user = new User("Daniel", "Namur");
+        User user1 = new User("Daniel", "Namur");
+        User user2 = new User("Daniel", "Namur");
+
+        // List<User> users = new ArrayList<>();
+        // users.add(user);
+        // users.add(user1);
+        // users.add(user2);
+
+        List<User> users = Arrays.asList(user, user1, user2);
+
+        return users;
+    }
+    
 }
