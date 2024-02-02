be43cfbb014a094ba296ee42e22bb0af7f45883a
Danamur <d.namur.n@gmail.com>
2024-02-02 13:09:39 -0300

    Utilizando foreach en thymeleaf para mostrar una lista de usuaruia

--- springboot-webapp/src/main/java/com/danielnamur/curso/springboot/webapp/springbootwebapp/controllers/UserController.java
+++ springboot-webapp/src/main/java/com/danielnamur/curso/springboot/webapp/springbootwebapp/controllers/UserController.java
@@ -1,10 +1,15 @@
 package com.danielnamur.curso.springboot.webapp.springbootwebapp.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import models.User;
+
 
 @Controller
 public class UserController {
@@ -20,4 +25,17 @@ public class UserController {
         return "details";
     }
 
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+
+        User user = new User("Daniel", "Namur");
+        User user1 = new User("Daniel", "Namur");
+        User user2 = new User("Daniel", "Namur");
+
+        List<User> users = Arrays.asList(user, user1, user2);
+
+        model.addAttribute("users", users);
+        model.addAttribute("title", "Lista de usuarios");
+        return "list";
+    }
 }
--- springboot-webapp/src/main/java/models/User.java
+++ springboot-webapp/src/main/java/models/User.java
@@ -6,10 +6,19 @@ public class User {
     private String lastname;
     private String email;
     
+    public User() {
+    }
+
     public User(String name, String lastname) {
         this.name = name;
         this.lastname = lastname;
     }
+    
+    public User(String name, String lastname, String email) {
+        this(name, lastname);
+        this.email = email;
+    }
+
     public String getName() {
         return name;
     }
