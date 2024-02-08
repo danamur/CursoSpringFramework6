4ec35b69a40fe0d870534e8010d23c69f8df7529
Danamur <d.namur.n@gmail.com>
2024-02-08 11:55:51 -0300

    Integrando anotacion RequestParam para enviar mensaje personalizado por url o mostrar un mensaje por defecto

---
+++ springboot-webapp/src/main/java/com/danielnamur/curso/springboot/webapp/springbootwebapp/controllers/RequestParamsController.java
@@ -0,0 +1,21 @@
+package com.danielnamur.curso.springboot.webapp.springbootwebapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import models.dto.ParamDto;
+
+@RestController
+@RequestMapping("/api/params")
+public class RequestParamsController {
+
+    @GetMapping("/foo")
+    public ParamDto foo(@RequestParam(required = false, defaultValue = "Hola Mundo") String message) {
+        ParamDto param = new ParamDto();
+        param.setMessage(message);
+        return param;
+    }
+
+}
