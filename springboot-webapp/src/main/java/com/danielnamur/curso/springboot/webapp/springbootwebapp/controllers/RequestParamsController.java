76bca6ef6577c897398d09aa535f8dadc1bc7863
Danamur <d.namur.n@gmail.com>
2024-02-08 12:47:12 -0300

    Utilizamos Objeto Request desde el HttpServletRequest

--- springboot-webapp/src/main/java/com/danielnamur/curso/springboot/webapp/springbootwebapp/controllers/RequestParamsController.java
+++ springboot-webapp/src/main/java/com/danielnamur/curso/springboot/webapp/springbootwebapp/controllers/RequestParamsController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.servlet.http.HttpServletRequest;
 import models.dto.ParamDto;
 
 @RestController
@@ -23,6 +24,20 @@ public class RequestParamsController {
         ParamDto params = new ParamDto();
         params.setMessage(text);
         params.setCode(code);
+        return params;
+    }
+    
+    @GetMapping("/request")
+    public ParamDto request(HttpServletRequest request) {
+        Integer code;
+        try {
+            code = Integer.parseInt(request.getParameter("code"));
+        } catch (NumberFormatException e) {
+            code = 0;
+        }
+        ParamDto params = new ParamDto();
+        params.setCode(code);
+        params.setMessage(request.getParameter("message"));
         return params;
     }
     
