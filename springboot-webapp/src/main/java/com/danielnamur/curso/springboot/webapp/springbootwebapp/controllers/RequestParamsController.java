f3fb661d235f00823e9721de0f409ab5989db9f6
Danamur <d.namur.n@gmail.com>
2024-02-08 12:29:22 -0300

    Entrgando datos por url con multiples parametros

--- springboot-webapp/src/main/java/com/danielnamur/curso/springboot/webapp/springbootwebapp/controllers/RequestParamsController.java
+++ springboot-webapp/src/main/java/com/danielnamur/curso/springboot/webapp/springbootwebapp/controllers/RequestParamsController.java
@@ -18,4 +18,13 @@ public class RequestParamsController {
         return param;
     }
 
+    @GetMapping("/bar")
+    public ParamDto bar(@RequestParam String text, @RequestParam Integer code) {
+        ParamDto params = new ParamDto();
+        params.setMessage(text);
+        params.setCode(code);
+        return params;
+    }
+    
+
 }
--- springboot-webapp/src/main/java/models/dto/ParamDto.java
+++ springboot-webapp/src/main/java/models/dto/ParamDto.java
@@ -3,6 +3,7 @@ package models.dto;
 public class ParamDto {
 
     private String message;
+    private Integer code;
 
     public String getMessage() {
         return message;
@@ -10,6 +11,14 @@ public class ParamDto {
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
     }
 
 }
