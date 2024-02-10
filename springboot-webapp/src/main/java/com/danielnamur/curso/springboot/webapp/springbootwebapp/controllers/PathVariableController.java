3bcdea43c18b83a7024bf75ed5ae3fbb07cbd4f6
Danamur <d.namur.n@gmail.com>
2024-02-10 13:37:20 -0300

    Enviando json por peticion POST y capitalizacion del nombre y apellido

--- springboot-webapp/src/main/java/com/danielnamur/curso/springboot/webapp/springbootwebapp/controllers/PathVariableController.java
+++ springboot-webapp/src/main/java/com/danielnamur/curso/springboot/webapp/springbootwebapp/controllers/PathVariableController.java
@@ -3,6 +3,7 @@ package com.danielnamur.curso.springboot.webapp.springbootwebapp.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import models.User;
 import models.dto.ParamDto;
 
 import java.util.HashMap;
@@ -10,6 +11,8 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("/api/var")
@@ -31,5 +34,25 @@ public class PathVariableController {
         return json;
     }
     
+    @PostMapping("/create")
+    public User create(@RequestBody User user) {
+        // Obtenemos el nombre y apellido del usuario
+        String name = user.getName();
+        String lastname = user.getLastname();
+
+        // Verificamos si el nombre esta vacio
+        if (!name.isEmpty()) {
+            String capitalizedName = name.substring(0,1).toUpperCase() + name.substring(1);
+            user.setName(capitalizedName);
+        }
+
+        //Verificamos si el apellido esta vacio
+        if (!lastname.isEmpty()) {
+            String capitalizedLastname = lastname.substring(0,1).toUpperCase() + lastname.substring(1);
+            user.setLastname(capitalizedLastname);
+        }
+
+        return user;
+    }
     
 }
