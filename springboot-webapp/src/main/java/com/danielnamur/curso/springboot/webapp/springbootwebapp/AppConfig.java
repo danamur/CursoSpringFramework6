19d47250e3bf8695af017598b1271e2c6b76d6c6
Danamur <d.namur.n@gmail.com>
2024-02-10 22:59:49 -0300

    Agregando archivos independientes para los properties personalizados para la configuracion

---
+++ springboot-webapp/src/main/java/com/danielnamur/curso/springboot/webapp/springbootwebapp/AppConfig.java
@@ -0,0 +1,10 @@
+package com.danielnamur.curso.springboot.webapp.springbootwebapp;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("classpath:values.properties")
+public class AppConfig {
+
+}
