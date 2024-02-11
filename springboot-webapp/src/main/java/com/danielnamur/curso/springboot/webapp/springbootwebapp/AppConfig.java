7c001186be1b9bc975c872fb12fcce3d47ad38a6
Danamur <d.namur.n@gmail.com>
2024-02-10 23:07:17 -0300

    Utilizando encoding charset para los properties

--- springboot-webapp/src/main/java/com/danielnamur/curso/springboot/webapp/springbootwebapp/AppConfig.java
+++ springboot-webapp/src/main/java/com/danielnamur/curso/springboot/webapp/springbootwebapp/AppConfig.java
@@ -2,9 +2,12 @@ package com.danielnamur.curso.springboot.webapp.springbootwebapp;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @Configuration
-@PropertySource("classpath:values.properties")
+@PropertySources({
+    @PropertySource(value="classpath:values.properties", encoding="UTF-8")
+})
 public class AppConfig {
 
 }
