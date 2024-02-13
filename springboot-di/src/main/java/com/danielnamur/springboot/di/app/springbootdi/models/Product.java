2172cca7db37f0ca7aa272c469463056230e088e
Danamur <d.namur.n@gmail.com>
2024-02-13 19:26:17 -0300

    Creando las clase del modelo y repositorio junto con una lista de productos

---
+++ springboot-di/src/main/java/com/danielnamur/springboot/di/app/springbootdi/models/Product.java
@@ -0,0 +1,42 @@
+package com.danielnamur.springboot.di.app.springbootdi.models;
+
+public class Product {
+
+    private Long id_product;
+    private String name;
+    private Long price;
+
+    public Product() {
+    }
+
+    public Product(Long id_product, String name, Long price) {
+        this.id_product = id_product;
+        this.name = name;
+        this.price = price;
+    }
+
+    public Long getId_product() {
+        return id_product;
+    }
+
+    public void setId_product(Long id_product) {
+        this.id_product = id_product;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+    
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+}
---
+++ springboot-di/src/main/java/com/danielnamur/springboot/di/app/springbootdi/repositories/ProductRepository.java
@@ -0,0 +1,31 @@
+package com.danielnamur.springboot.di.app.springbootdi.repositories;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.danielnamur.springboot.di.app.springbootdi.models.Product;
+
+public class ProductRepository {
+
+    private List<Product> data;
+
+    public ProductRepository() {
+        this.data = Arrays.asList(
+            new Product(1L, "Procesador Intel Core i5", 150000L),
+            new Product(2L, "Memoria RAM DDR4 8GB", 50000L),
+            new Product(3L, "Disco Duro SSD 256GB", 70000L),
+            new Product(4L, "Tarjeta Gráfica NVIDIA GTX 1660", 250000L),
+            new Product(5L, "Placa Madre ASUS B450", 100000L),
+            new Product(6L, "Fuente de Poder 600W", 60000L),
+            new Product(7L, "Gabinete ATX RGB", 80000L),
+            new Product(8L, "Teclado Mecánico RGB", 60000L),
+            new Product(9L, "Mouse Gamer con Cable", 40000L),
+            new Product(10L, "Monitor LED 24 Pulgadas", 150000L)
+        );
+    }
+
+    public List<Product> findAll() {
+        return data;
+    }
+
+}
