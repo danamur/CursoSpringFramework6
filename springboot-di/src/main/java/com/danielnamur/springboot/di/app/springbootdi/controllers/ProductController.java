d672cc1a51a1c3eaca1df8fa8f533d3a870498a8
Danamur <d.namur.n@gmail.com>
2024-02-17 16:42:39 -0300

    Creando la clase Controller para nuestra api products

---
+++ springboot-di/src/main/java/com/danielnamur/springboot/di/app/springbootdi/controllers/ProductController.java
@@ -0,0 +1,28 @@
+package com.danielnamur.springboot.di.app.springbootdi.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.danielnamur.springboot.di.app.springbootdi.models.Product;
+import com.danielnamur.springboot.di.app.springbootdi.services.ProductService;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    private ProductService service = new ProductService();
+
+    @GetMapping()
+    public List<Product> list() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Product show(@PathVariable Long id) {
+        return service.findById(id);
+    }
+}
