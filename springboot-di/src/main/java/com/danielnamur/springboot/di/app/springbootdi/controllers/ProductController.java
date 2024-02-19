773e4954c6d0f35f151ffe52fabeb523c23a9f72
Danamur <d.namur.n@gmail.com>
2024-02-19 12:32:21 -0300

    Inyeccion mediante la interface

--- springboot-di/src/main/java/com/danielnamur/springboot/di/app/springbootdi/controllers/ProductController.java
+++ springboot-di/src/main/java/com/danielnamur/springboot/di/app/springbootdi/controllers/ProductController.java
@@ -9,14 +9,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.danielnamur.springboot.di.app.springbootdi.models.Product;
-import com.danielnamur.springboot.di.app.springbootdi.services.ProductServiceImpl;
+import com.danielnamur.springboot.di.app.springbootdi.services.ProductService;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
 
     @Autowired
-    private ProductServiceImpl service;
+    private ProductService service;
 
     @GetMapping()
     public List<Product> list() {
--- springboot-di/src/main/java/com/danielnamur/springboot/di/app/springbootdi/services/ProductServiceImpl.java
+++ springboot-di/src/main/java/com/danielnamur/springboot/di/app/springbootdi/services/ProductServiceImpl.java
@@ -7,13 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.danielnamur.springboot.di.app.springbootdi.models.Product;
-import com.danielnamur.springboot.di.app.springbootdi.repositories.ProductRepositoryImpl;
+import com.danielnamur.springboot.di.app.springbootdi.repositories.ProductRepository;
 
 @Component
 public class ProductServiceImpl implements ProductService {
 
     @Autowired
-    private ProductRepositoryImpl repository;
+    private ProductRepository repository;
 
     /**
      * Retorna una lista de todos los productos con precios convertidos.
