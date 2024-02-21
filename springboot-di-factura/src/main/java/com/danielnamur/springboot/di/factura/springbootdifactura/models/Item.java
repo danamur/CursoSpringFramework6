26288a08014db81fbcf3df3ccaae64eb3b41914c
Danamur <d.namur.n@gmail.com>
2024-02-21 13:58:40 -0300

    Creando relacion entre los componentes

--- springboot-di-factura/src/main/java/com/danielnamur/springboot/di/factura/springbootdifactura/models/Item.java
+++ springboot-di-factura/src/main/java/com/danielnamur/springboot/di/factura/springbootdifactura/models/Item.java
@@ -2,4 +2,20 @@ package com.danielnamur.springboot.di.factura.springbootdifactura.models;
 
 public class Item {
 
+    private Product product;
+    private Integer quantity;
+
+    public Product getProduct() {
+        return product;
+    }
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+    public Integer getQuantity() {
+        return quantity;
+    }
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
 }
