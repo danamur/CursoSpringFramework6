d10f0da3137899ac6515701e3a2a273bc92fefd4
Danamur <d.namur.n@gmail.com>
2024-03-05 20:15:20 -0300

    Personalizando error 404

--- springboot-error/src/main/java/com/danielnamur/springboot/error/springbooterror/controllers/HandlerExceptionController.java
+++ springboot-error/src/main/java/com/danielnamur/springboot/error/springbooterror/controllers/HandlerExceptionController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.danielnamur.springboot.error.springbooterror.models.Error;
 
@@ -22,5 +23,16 @@ public class HandlerExceptionController {
         
         //return ResponseEntity.internalServerError().body(error);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR.value()).body(error);
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<Error> notFound(Exception exception) {
+        Error error = new Error();
+        error.setDate(new Date());
+        error.setError("Api no encontrada");
+        error.setMessage(exception.getMessage());
+        error.setStatus(HttpStatus.NOT_FOUND.value());
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND.value()).body(error);
     }
 }
