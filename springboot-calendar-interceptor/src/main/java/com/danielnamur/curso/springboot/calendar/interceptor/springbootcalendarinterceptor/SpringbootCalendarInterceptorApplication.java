89cdd04ebad263c6654f459155d87566589e1d8f
Danamur <d.namur.n@gmail.com>
2024-03-13 13:44:02 -0300

    Creando proyecto de Interceptor HTTP Horario de acceso de cliente
    Se implemento un controlador basico de respuesta y archivo de properties para el control de apertura y cierre de la aplicacion

---
+++ springboot-calendar-interceptor/src/main/java/com/danielnamur/curso/springboot/calendar/interceptor/springbootcalendarinterceptor/SpringbootCalendarInterceptorApplication.java
@@ -0,0 +1,13 @@
+package com.danielnamur.curso.springboot.calendar.interceptor.springbootcalendarinterceptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCalendarInterceptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCalendarInterceptorApplication.class, args);
+	}
+
+}
---
+++ springboot-calendar-interceptor/src/test/java/com/danielnamur/curso/springboot/calendar/interceptor/springbootcalendarinterceptor/SpringbootCalendarInterceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielnamur.curso.springboot.calendar.interceptor.springbootcalendarinterceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCalendarInterceptorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
