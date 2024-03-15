e89bd3f8aec9bbf7a9a260ad2fa3336460640436
Danamur <d.namur.n@gmail.com>
2024-03-15 07:56:22 -0300

    Implementando hora de cierre en el interceptor

--- springboot-calendar-interceptor/src/main/java/com/danielnamur/curso/springboot/calendar/interceptor/springbootcalendarinterceptor/interceptors/CalendarInterceptor.java
+++ springboot-calendar-interceptor/src/main/java/com/danielnamur/curso/springboot/calendar/interceptor/springbootcalendarinterceptor/interceptors/CalendarInterceptor.java
@@ -1,12 +1,18 @@
 package com.danielnamur.curso.springboot.calendar.interceptor.springbootcalendarinterceptor.interceptors;
 
 import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -26,6 +32,8 @@ public class CalendarInterceptor implements HandlerInterceptor {
         Calendar calendar = Calendar.getInstance();
         int hour = calendar.get(Calendar.HOUR_OF_DAY);
 
+        System.out.println(hour);
+
         if (hour >= open && hour < close) {
             StringBuilder message = new StringBuilder("Bienvenido al portal!");
             message.append("Se encuentra abierto desde las ");
@@ -38,6 +46,22 @@ public class CalendarInterceptor implements HandlerInterceptor {
             return true;
         }
 
+        ObjectMapper mapper = new ObjectMapper();
+        Map<String, Object> data = new HashMap<>();
+
+        StringBuilder message = new StringBuilder("Cerrado, fuera del horario de atencion ");
+        message.append("por favor, vuelva desde las ");
+        message.append(open);
+        message.append(" y las ");
+        message.append(close);
+        message.append(" hrs.");
+        
+        data.put("message", message.toString());
+        data.put("date", new Date());
+
+        response.setContentType("application/json");
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response .getWriter().write(mapper.writeValueAsString(data));
         return false;
     }
 
